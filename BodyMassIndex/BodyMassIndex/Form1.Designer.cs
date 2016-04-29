7ab06b03e07d8f6f608dbc4d7ffd62b41718d946
Michael Leontieff-Smith <michael@leontieff.net>
2016-04-29 22:32:29 +1000

    Added source files

---
+++ BodyMassIndex/BodyMassIndex/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BodyMassIndex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWeightInput = new System.Windows.Forms.TextBox();
+            this.txtHeightInput = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.gbAnotherCalc = new System.Windows.Forms.GroupBox();
+            this.rbCalcNo = new System.Windows.Forms.RadioButton();
+            this.rbCalcYes = new System.Windows.Forms.RadioButton();
+            this.gbAnotherCalc.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Body Mass Index Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Weight (kg)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(169, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Height (m)";
+            // 
+            // txtWeightInput
+            // 
+            this.txtWeightInput.Location = new System.Drawing.Point(29, 101);
+            this.txtWeightInput.Name = "txtWeightInput";
+            this.txtWeightInput.Size = new System.Drawing.Size(84, 20);
+            this.txtWeightInput.TabIndex = 3;
+            // 
+            // txtHeightInput
+            // 
+            this.txtHeightInput.Location = new System.Drawing.Point(172, 101);
+            this.txtHeightInput.Name = "txtHeightInput";
+            this.txtHeightInput.Size = new System.Drawing.Size(84, 20);
+            this.txtHeightInput.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(105, 141);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 58);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate BMI";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // gbAnotherCalc
+            // 
+            this.gbAnotherCalc.Controls.Add(this.rbCalcNo);
+            this.gbAnotherCalc.Controls.Add(this.rbCalcYes);
+            this.gbAnotherCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbAnotherCalc.Location = new System.Drawing.Point(29, 213);
+            this.gbAnotherCalc.Name = "gbAnotherCalc";
+            this.gbAnotherCalc.Size = new System.Drawing.Size(227, 80);
+            this.gbAnotherCalc.TabIndex = 6;
+            this.gbAnotherCalc.TabStop = false;
+            this.gbAnotherCalc.Text = "Another Calculation?";
+            this.gbAnotherCalc.Visible = false;
+            // 
+            // rbCalcNo
+            // 
+            this.rbCalcNo.AutoSize = true;
+            this.rbCalcNo.Location = new System.Drawing.Point(143, 38);
+            this.rbCalcNo.Name = "rbCalcNo";
+            this.rbCalcNo.Size = new System.Drawing.Size(46, 20);
+            this.rbCalcNo.TabIndex = 1;
+            this.rbCalcNo.Text = "No";
+            this.rbCalcNo.UseVisualStyleBackColor = true;
+            this.rbCalcNo.CheckedChanged += new System.EventHandler(this.rbCalcNo_CheckedChanged);
+            // 
+            // rbCalcYes
+            // 
+            this.rbCalcYes.AutoSize = true;
+            this.rbCalcYes.Location = new System.Drawing.Point(19, 38);
+            this.rbCalcYes.Name = "rbCalcYes";
+            this.rbCalcYes.Size = new System.Drawing.Size(53, 20);
+            this.rbCalcYes.TabIndex = 0;
+            this.rbCalcYes.Text = "Yes";
+            this.rbCalcYes.UseVisualStyleBackColor = true;
+            this.rbCalcYes.CheckedChanged += new System.EventHandler(this.rbCalcYes_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 325);
+            this.Controls.Add(this.gbAnotherCalc);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeightInput);
+            this.Controls.Add(this.txtWeightInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Body Mass Index";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbAnotherCalc.ResumeLayout(false);
+            this.gbAnotherCalc.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWeightInput;
+        private System.Windows.Forms.TextBox txtHeightInput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.GroupBox gbAnotherCalc;
+        private System.Windows.Forms.RadioButton rbCalcNo;
+        private System.Windows.Forms.RadioButton rbCalcYes;
+    }
+}
+
---
+++ BodyMassIndex/BodyMassIndex/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BodyMassIndex
+{
+    /// <summary>
+    /// --------------------CLASS COMMENT--------------------
+    /// Name:               Michael Leontieff-Smith
+    /// Student ID:         N9455396
+    /// 
+    /// Name: Body Mass Index
+    /// 
+    /// Calculates the body mass index of the user using supplied 
+    /// information then correlates that with predefined states such
+    /// as under and overweight.
+    /// 
+    /// </summary>
+    public partial class Form1 : Form
+    {
+         // function parameter variables 
+        const int WEIGHT = 0;
+        const int HEIGHT = 1;
+        const double INVALIDATED_OUTPUT = -1;
+        const int STATES_COUNT = 4;
+        // max values
+        const double MIN_WEIGHT = 45;
+        const double MIN_HEIGHT = 1.2;
+        const double MAX_HEIGHT = 2.7;
+        // BMI State maximums
+        const double MAX_UNDERWEIGHT = 18.5;
+        const double MAX_HEALTHY = 24.99;
+        const double MAX_OVERWEIGHT = 29.99;
+        // declare enum for naming BMI ranges, these form part of the output string
+        enum state { Underweight, Healthy, Overweight, Obese}
+
+
+        /// <summary>
+        /// Calculates BMI using supplied values that have been converted to 
+        /// doubles
+        /// </summary>
+        /// <param name="weight_input">double weight input</param>
+        /// <param name="height_input">double height input </param>
+        /// <returns>returns the double BMI value</returns>
+        static double CalculateBMI(double weight_input, double height_input)
+        {
+            double bodyMI = 0;
+            bodyMI = weight_input / (height_input * height_input);
+            return bodyMI;
+        }
+
+        /// <summary>
+        /// accepts BMI as parameter and returns appropriate enum
+        /// </summary>
+        /// <param name="bodyMI">calculated BMI</param>
+        /// <returns>corresponding enum bassed on which range the BMI resides within</returns>
+        static state CalculateStatus(double bodyMI)
+        {
+            if (bodyMI < MAX_UNDERWEIGHT)
+            {
+                return state.Underweight;
+            }
+            else if (bodyMI >= MAX_UNDERWEIGHT && bodyMI <= MAX_HEALTHY)
+            {
+                return state.Healthy;
+            }
+            else if (bodyMI > MAX_HEALTHY && bodyMI < MAX_OVERWEIGHT)
+            {
+                return state.Overweight;
+            }
+            else
+            {
+                return state.Obese;
+            }
+        }
+
+        /// <summary>
+        /// Converts the input to double and presents error if invalid data entered
+        /// </summary>
+        /// <param name="user_input">string input from textbox</param>
+        /// <returns>user input converted to double</returns>
+        static double ConvertInput(string user_input)
+        {
+            double validated_output;
+
+                if (!double.TryParse(user_input, out validated_output))
+                {
+                    MessageBox.Show("Error! Please Enter Numeric Value");
+                }
+                else
+                {
+                    return validated_output; 
+                }
+
+            return INVALIDATED_OUTPUT;
+        }
+
+        /// <summary>
+        /// Accepts the double-converted user input and checkes whether it's within bounds
+        /// </summary>
+        /// <param name="value">the double user value</param>
+        /// <param name="type">used to check whether the value is for weight or height</param>
+        /// <returns>true or false depending on whether it's valid or not for the respective boundary</returns>
+        static bool VerifyBounds(double value, int type)
+        {
+            if (type == WEIGHT)
+            {
+                if (value >= MIN_WEIGHT)
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Weight value below minimum of 45kg");
+                }
+            }
+            // Height is the only other option
+            else
+            {
+                if (value <= MAX_HEIGHT && value >= MIN_HEIGHT)
+                {
+                    return true;
+                }
+                else if (value > MAX_HEIGHT)
+                {
+                    MessageBox.Show("Height is above the max of 2.7m");
+                }
+                else
+                {
+                    MessageBox.Show("Height is below the minimum of 1.2m");
+                }
+            }
+            return false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// runs functions required to calculate input then formulate a string and
+        /// display a textbox
+        /// </summary>
+        /// <param name="weight_input">verified within boundary weight double</param>
+        /// <param name="height_input">verified within boundary height double</param>
+        static void Output_BMI(double weight_input, double height_input)
+        {
+            double result;
+            string BMIoutput;
+            // calculate BMI
+            result = CalculateBMI(weight_input, height_input);
+            result = Math.Round(result, 2);
+            // convert result to string for display
+            BMIoutput = "Your BMI is '" + result.ToString() + "' indicating that you are " + CalculateStatus(result);
+            MessageBox.Show(BMIoutput);
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // doubles to store variables for parsing to BMI function
+            double weight_input, height_input;
+
+            // process input
+            weight_input = ConvertInput(txtWeightInput.Text);
+            height_input = ConvertInput(txtHeightInput.Text);
+
+
+            // pass the verified double values to the verify bounds method
+            if (weight_input != INVALIDATED_OUTPUT && height_input != INVALIDATED_OUTPUT)
+            {
+
+                if (!VerifyBounds(weight_input, WEIGHT) || !VerifyBounds(height_input, HEIGHT))
+                {
+                    rbCalcNo.Checked = false;
+                    rbCalcYes.Checked = false;
+                }
+                else
+                {
+                    Output_BMI(weight_input, height_input);
+                    rbCalcNo.Checked = false;
+                    rbCalcYes.Checked = false;
+                }
+            }
+            // Enable visibility of group box
+            gbAnotherCalc.Visible = true;
+
+            // Disable text fields and button
+            txtHeightInput.Enabled = false;
+            txtWeightInput.Enabled = false;
+            btnCalculate.Enabled = false;
+
+            // un-check 'yes' if already checked
+            rbCalcYes.Checked = false;
+        }
+
+        private void rbCalcYes_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbCalcYes.Checked)
+            {
+                // Clear text fields
+                txtHeightInput.Text = string.Empty;
+                txtWeightInput.Text = string.Empty;
+                // Re-Enable text fields and button
+                txtHeightInput.Enabled = true;
+                txtWeightInput.Enabled = true;
+                btnCalculate.Enabled = true;
+                // Hide group box
+                gbAnotherCalc.Visible = false;
+            }
+        }
+
+        private void rbCalcNo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbCalcNo.Checked)
+            {
+                MessageBox.Show("That's all folks", "Exit Program");
+                this.Close();
+            }
+            
+        }
+    }
+}
